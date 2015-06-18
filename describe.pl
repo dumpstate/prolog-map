ab4cececa81da7084c888559174dd6557ef597cd
dumpstate <dumpstate@outlook.com>
2015-06-19 00:07:21 +0200

    Add more doors

--- describe.pl
+++ describe.pl
@@ -3,39 +3,47 @@
 ]).
 
 describe(red_room) :-
-  write('You are in the Red room (red_room).'), nl,
-  write('\teast - lower_corridor'), nl.
+  write('You are in the Red room.'), nl,
+  write('\teast\t- lower corridor'), nl,
+  write('\tnorth\t- blue room'), nl.
 
 describe(green_room) :-
-  write('You are in the Green room (green_room).'), nl,
-  write('\twest - lower_corridor'), nl.
+  write('You are in the Green room.'), nl,
+  write('\twest\t- lower corridor'), nl,
+  write('\tnorth\t- blue room'), nl.
 
 describe(blue_room) :-
-  write('You are in the Blue room (blue_room).'), nl,
-  write('\tsouth - lower_corridor'), nl.
+  write('You are in the Blue room.'), nl,
+  write('\tsouth\t- lower corridor'), nl,
+  write('\twest\t- red room'), nl,
+  write('\teast\t- green room'), nl.
 
 describe(cyan_room) :-
-  write('You are in the Cyan room (cyan_room).'), nl,
-  write('\teast - upper_corridor'), nl.
+  write('You are in the Cyan room.'), nl,
+  write('\teast\t- upper corridor'), nl,
+  write('\tnorth\t- magenta room'), nl.
 
 describe(yellow_room) :-
-  write('You are in the Yellow room (yellow_room).'), nl,
-  write('\twest - upper_corridor'), nl.
+  write('You are in the Yellow room.'), nl,
+  write('\twest\t- upper corridor'), nl,
+  write('\tnorth\t- magenta room'), nl.
 
 describe(magenta_room) :-
-  write('You are in the Magenta room (magenta_room).'), nl,
-  write('\tsouth - upper_corridor'), nl.
+  write('You are in the Magenta room.'), nl,
+  write('\tsouth\t- upper corridor'), nl,
+  write('\twest\t- cyan room'), nl,
+  write('\teast\t- yellow room'), nl.
 
 describe(lower_corridor) :-
-  write('You are in the corridor at the ground floor (lower_corridor). '), nl,
-  write('\twest\t- red_room'), nl,
-  write('\tnorth\t- blue_room'), nl,
-  write('\teast\t- green_room'), nl,
-  write('\tup\t- upper_corridor'), nl.
+  write('You are in the corridor at the ground floor.'), nl,
+  write('\twest\t- red room'), nl,
+  write('\tnorth\t- blue room'), nl,
+  write('\teast\t- green room'), nl,
+  write('\tup\t- upper corridor'), nl.
 
 describe(upper_corridor) :-
-  write('You are in the corridor at the second floor (upper_corridor).'), nl,
-  write('\twest\t- cyan_room'), nl,
-  write('\tnorth\t- magenta_room'), nl,
-  write('\teast\t- yellow_room'), nl,
-  write('\tdown\t- lower_corridor'), nl.
+  write('You are in the corridor at the second floor.'), nl,
+  write('\twest\t- cyan room'), nl,
+  write('\tnorth\t- magenta room'), nl,
+  write('\teast\t- yellow room'), nl,
+  write('\tdown\t- lower corridor'), nl.
--- door.pl
+++ door.pl
@@ -4,13 +4,21 @@
 
 door(lower_corridor, west, red_room).
 door(red_room, east, lower_corridor).
+door(red_room, north, blue_room).
+door(blue_room, west, red_room).
 door(lower_corridor, north, blue_room).
 door(blue_room, south, lower_corridor).
 door(lower_corridor, east, green_room).
 door(green_room, west, lower_corridor).
+door(green_room, north, blue_room).
+door(blue_room, east, green_room).
 door(upper_corridor, west, cyan_room).
 door(cyan_room, east, upper_corridor).
+door(cyan_room, north, magenta_room).
+door(magenta_room, west, cyan_room).
 door(upper_corridor, north, magenta_room).
 door(magenta_room, south, upper_corridor).
 door(upper_corridor, east, yellow_room).
 door(yellow_room, west, upper_corridor).
+door(yellow_room, north, magenta_room).
+door(magenta_room, east, yellow_room).
